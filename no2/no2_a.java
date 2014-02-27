6c7932f6be2e0569cef41c88a90b22c7dd61bcca
snake4123 <bul0078@mail4.doshisha.ac.jp>
2014-02-27 16:37:00 +0900

    test

--- no2/no2_a.java
+++ no2/no2_a.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
 
 public class no2_a {
 
+	public class MyStack {
+		ArrayList<Integer> stack = new ArrayList<Integer>();
+		public void push( int value ) {
+			stack.add(value);
+		}
+
+		public int pop( ) {
+			int rtn;
+			if( stack.isEmpty() ) {
+				System.out.println( "スタックは空です." );
+				System.exit( 1 );
+			}
+
+			rtn = stack.get( 0 );
+			stack.remove( 0 );
+
+			return rtn;
+		}
+		public void peek() {
+			int rtn;
+			rtn = stack.get(0);
+		}
+	}
+
+		/*public boolean empty();
+		public int peek();
+		public int pop() ;
+		public int push(int value);
+		public int size();*/
+
 }
+
---
+++ no3/no3.java
@@ -0,0 +1,4 @@
+
+public class no3 {
+
+}
