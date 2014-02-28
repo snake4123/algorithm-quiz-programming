87aec75f98960eadc629360ffc432aed207aa1c4
snake4123 <bul0078@mail4.doshisha.ac.jp>
2014-02-28 15:43:30 +0900

    fix

--- no4/no4.java
+++ no4/no4.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class no4 {
 	static int minus (int a,int b){
 		int c = 0;
-		b = -b;
+		b = ~b + 1;//ビット反転によりマイナスを実現
 		c = a + b;
 		return c;
 	}
@@ -31,6 +31,10 @@ public class no4 {
 	static int divide(int a, int b){
 		int c = a;
 		int count = 0;
+		if (b == 0){
+			b = 100;
+			count = 999;
+		}
 		if(a<0&&b<0){
 			a = Math.abs(a);		//両方がマイナスならば，答えはプラスになるようにする
 			b = Math.abs(b);
@@ -53,22 +57,22 @@ public class no4 {
 			c = a;
 			for(;;){
 				c = minus(c,b);
-				count++;
 				if(c<=0){
 					count = -count;
 					break;
 				}
+				count++;
 			}
 		}
 		if(b<0){
 			b = Math.abs(b);		//片方がマイナスならば，答えはマイナスになるようにする
 			for(;;){
 				c = minus(c,b);
-				count++;
 				if(c<=0){
 					count = -count;
 					break;
 				}
+				count++;
 			}
 		}
 		return count;
@@ -82,8 +86,11 @@ public class no4 {
 		str = br.readLine();
 		int b = Integer.parseInt(str);
 		
-	System.out.println("減算："+minus(a,b));
-	System.out.println("乗算："+multiply(a,b));
-	System.out.println("除算："+divide(a,b)); 
+		System.out.println("減算："+minus(a,b));
+		System.out.println("乗算："+multiply(a,b));
+		if(Math.abs(divide(a,b))>=999){
+			System.out.println("除算：0で割れません"); 
+		}
+		else System.out.println("除算："+divide(a,b)); 
 	}	
 }
